44dea7cebccbfbfd5215432f2f36bc80c5ac0193
dsu784 <nguyen.huynh>
2024-07-15 17:32:07 +0700

    update

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import BookShow from './BookShow';
 
-function BookList() {
-  return (
-    <div>BookList</div>
-  )
+function BookList({ books, onDelete, onEdit }) {
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    );
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
-export default BookList
+export default BookList;
